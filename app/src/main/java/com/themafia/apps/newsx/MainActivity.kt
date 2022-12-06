04a39e3138e040aad2558460fc325f11875975d6
The Mafia <issactoluwanimi@gmail.com>
2022-12-06 14:59:21 -0800

    Added Nav Graph and stuffs

--- app/src/main/java/com/themafia/apps/newsx/MainActivity.kt
+++ app/src/main/java/com/themafia/apps/newsx/MainActivity.kt
@@ -2,13 +2,26 @@ package com.themafia.apps.newsx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.themafia.apps.newsx.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityMainBinding
+
+    lateinit var navHostFragment : NavHostFragment
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+
     }
 }
